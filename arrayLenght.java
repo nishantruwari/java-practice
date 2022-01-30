0ead4be3cb99ae64069616787c951e8fd34586ec
nishantruwari <83151342+nishantruwari@users.noreply.github.com>
2022-01-30 10:21:29 +0530

    Add files via upload

---
+++ arrayLenght.java
@@ -0,0 +1,9 @@
+public class arrayLenght {
+
+    public static void main(String[] args) {
+        int[] t = new int[3];
+        int q = t.length;
+        System.out.println("Lenght" + q);
+    }
+
+}
